64e207f836346bb7b46b13eb4c545443578c0e66
GwangMyeong Seo <auddlzz000@gmail.com>
2024-07-09 14:06:43 +0000

    react 보안 설정

--- shop/frontend/src/App.tsx
+++ shop/frontend/src/App.tsx
@@ -3,12 +3,12 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [message, setMessage] = useState<string>('');
 
-  useEffect(() => {
-    fetch('http://localhost:8080/api/message')
-      .then(response => response.text())
-      .then(data => setMessage(data))
-      .catch(error => console.error('Error:', error));
-  }, []);
+  // useEffect(() => {
+  //   fetch('http://localhost:8080/api/message')
+  //     .then(response => response.text())
+  //     .then(data => setMessage(data))
+  //     .catch(error => console.error('Error:', error));
+  // }, []);
 
   return (
     <div className="App">
