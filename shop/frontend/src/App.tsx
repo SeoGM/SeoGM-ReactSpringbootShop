44f8fd8c8350327e5e4644b609832cbfb4d94a4c
GwangMyeong Seo <auddlzz000@gmail.com>
2024-07-15 08:00:41 +0000

    cors 에러 대응

--- shop/frontend/src/App.tsx
+++ shop/frontend/src/App.tsx
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [message, setMessage] = useState<string>('');
 
-  // useEffect(() => {
-  //   fetch('http://localhost:8080/api/message')
-  //     .then(response => response.text())
-  //     .then(data => setMessage(data))
-  //     .catch(error => console.error('Error:', error));
-  // }, []);
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'https://8080-seogm-seogmreactspring-w81oj40yfrx.ws-us115.gitpod.io';
+  
+  useEffect(() => {
+    fetch(`${backendUrl}/api/message`)
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
 
   return (
     <div className="App">
